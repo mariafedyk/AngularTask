f03f8af169f13baf76ba796037cabc6e7191e120
mariafedyk <51701368+mariafedyk@users.noreply.github.com>
2019-07-22 11:54:03 +0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiLink: 'http://www.mocky.io/v2/5d323594330000a9c17ba5f1',
+  apiLink: 'https://www.mocky.io/v2/5d323594330000a9c17ba5f1',
   production: false
 };
 
